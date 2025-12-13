1549bffec5864942c21af3b66d658822e6ecbefc
facsert <facsert@outlook.com>
2025-12-13 18:59:47 +0800

    Go
    1.fix bug of comm.struct function
    
    Signed-off-by: facsert <facsert@outlook.com>

--- comm/struct.go
+++ comm/struct.go
@@ -13,31 +13,46 @@ var (
 	UpdateTag TagType = "update"
 )
 
-func parseTag(field reflect.StructField, tag TagType) string {
+type fieldTag struct {
+	name string
+	ignore bool
+	omitZero bool
+}
+
+// 解析 Tag
+func parseTag(field reflect.StructField, tag TagType) *fieldTag {
 	jsonTag := field.Tag.Get(string(tag))
 	if jsonTag == "-" {
-		return ""
+		return &fieldTag{"", true, false}
 	}
-	if idx := strings.Index(jsonTag, ","); idx != -1 {
-		return jsonTag[:idx]
+	name, opts, _ := strings.Cut(jsonTag, ",")
+	if strings.TrimSpace(name) == "" {
+		name = field.Name
 	}
+	return &fieldTag{name, false, strings.Contains(opts, "omitzero")}
+}
 
-	if jsonTag != "" {
-		return jsonTag
+// 确保传入的是结构体类型
+func ensureStruct(m any) (reflect.Type, reflect.Value, bool){
+	t, v := reflect.TypeOf(m), reflect.ValueOf(m)
+	if t.Kind() == reflect.Pointer {
+		if v.IsNil() {
+			return nil, reflect.Value{}, false
+		}
+		t, v = t.Elem(), v.Elem()
 	}
-
-	return field.Name
+	if v.Kind() != reflect.Struct {
+		return nil, reflect.Value{}, false
+	}
+	return t, v, true
 }
 
 func StructKeys(m any, tags ...TagType) []string {
-	if m == nil {
+	t, v, isStruct := ensureStruct(m)
+	if !isStruct {
 		return nil
 	}
-	t := reflect.TypeOf(m)
-	if t.Kind() == reflect.Pointer {
-		t = t.Elem()
-	}
-
+	
 	tag := JsonTag
 	if len(tags) > 0 {
 		tag = tags[0]
@@ -45,54 +60,47 @@ func StructKeys(m any, tags ...TagType) []string {
 
 	keys := make([]string, 0, t.NumField())
 	for index := 0; index < t.NumField(); index++ {
-		if key := parseTag(t.Field(index), tag); key != "" {
-			keys = append(keys, key)
+		ft := parseTag(t.Field(index), tag)
+		if ft.ignore {
+			continue
 		}
+		if ft.omitZero && v.Field(index).IsZero() {
+			continue
+		}
+		keys = append(keys, ft.name)
 	}
 	return keys
 }
 
-func StructValue(m any, key string) any {
-	if m == nil {
-		return nil
-	}
-
-	t := reflect.TypeOf(m)
-	v := reflect.ValueOf(m)
-
-	if v.Kind() == reflect.Pointer {
-		t, v = t.Elem(), v.Elem()
-	}
-	for index := 0; index < t.NumField(); index++ {
-		if key == t.Field(index).Name {
-			return v.Field(index).Interface()
-		}
-	}
-	return nil
+func StructValue(m any, field string) any {
+    _, v, isStruct := ensureStruct(m)
+    if !isStruct {
+        return nil
+    }
+	return v.FieldByName(field).Interface()
 }
 
 func StructMap(m any, tags ...TagType) map[string]any {
-	if m == nil {
-		return nil
-	}
-
-	t := reflect.TypeOf(m)
-	v := reflect.ValueOf(m)
-
-	if v.Kind() == reflect.Pointer {
-		t, v = t.Elem(), v.Elem()
-	}
+	t, v, isStruct := ensureStruct(m)
+    if !isStruct {
+        return nil
+    }
 
 	tag := JsonTag
 	if len(tags) > 0 {
 		tag = tags[0]
 	}
 
-	mMap := make(map[string]any, t.NumField())
+	structMap := make(map[string]any, t.NumField())
 	for index := 0; index < t.NumField(); index++ {
-		if key := parseTag(t.Field(index), tag); key != "" {
-			mMap[key] = v.Field(index).Interface()
+		ft := parseTag(t.Field(index), tag)
+		if ft.ignore {
+			continue
 		}
+		if ft.omitZero && v.Field(index).IsZero() {
+			continue
+		}
+		structMap[ft.name] = v.Field(index).Interface()
 	}
-	return mMap
+	return structMap
 }
